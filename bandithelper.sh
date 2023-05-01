a9d0226a0b91df90961218ce448e886a0bad5227
ryoxao <131062882+ryoxao@users.noreply.github.com>
2023-05-01 21:34:50 +0200

    Add files via upload

--- bandithelper.sh
+++ bandithelper.sh
@@ -114,6 +114,7 @@ if [ $option -eq 2 ]
           echo -e "${purpleColour}Exactly, the conection would look lke : \"ssh bandit0@bandit.labs.overthewire.org -p 2220\" i'll lunch another terinal where i'll run this comand.${endColour}"  
           read
           echo $(gnome-terminal -e "/usr/bin/ssh bandit0@bandit.labs.overthewire.org -p 2220")
+          echo -e "${redColour}[+]${endColour} ${purpleColour}More information of the level $selectlevel in their website :${endColour} https://overthewire.org/wargames/bandit/bandit$selectlevel.html${blueColour}${endColour}"
           read -p "Waiting until you close the terminal of the conection with the bandit servers, When you will close it Press Enter..."
           selectlevel=""
         elif [ "$selectlevel" != "0" ]
